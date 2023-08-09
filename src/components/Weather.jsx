0cde51eb00b1d5ec332c6c42589e13b0ec2c972c
Milton Omar <milton.yv98@gmail.com>
2023-08-08 21:09:39 -0500

    Cambios Weather

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -23,16 +23,16 @@ const Weather = ({ weatherInfo }) => {
       <h2 className="text-[60px] ">{weatherInfo?.name}</h2>
       <section className="grid gap-4 sm:grid-cols-[auto_auto]">
         {/* Seccion superior */}
-        <section className="bg-white/20 backdrop-blur-sm p-2 rounded-2xl grid grid-cols-2 items-center">
+        <section className="bg-white/20 backdrop-blur-sm p-2 rounded-2xl grid grid-cols-2 items-center ">
           <h4 className="col-span-2 text-[40px]">{weatherInfo?.weather[0].description}</h4>
-          <span className="text-4xl">{tempSwitch}</span>
+          <span className="text-4xl sm:text-6xl">{tempSwitch}</span>
           <div>
             <img src={`https://openweathermap.org/img/wn/${weatherInfo?.weather[0].icon}@4x.png`} alt="" />
           </div>
         </section>
 
         {/* Seccion inferior */}
-        <section className=" bg-white/20 backdrop-blur-sm p-2 rounded-2xl grid grid-cols-3 items-center py-4 sm:grid-cols-1 text-[18px]">
+        <section className=" bg-white/20 backdrop-blur-sm p-2 rounded-2xl grid grid-cols-3 items-center py-4  sm:grid-cols-1 text-[17px] sm:text-[20px]">
           <article className="flex gap-2 items-center mx-auto ">
             <div className="w-[30px]">
                 <img src={"/images/wind.png"} alt="" />
